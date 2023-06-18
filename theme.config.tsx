a5561124b279186aea419aed48faf84fac18b3e1
Quynh Nguyen <quynh.nguyen@digitalfortress.dev>
2023-06-18 18:30:44 +0700

    feat: Initialize DF Foundation

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Foundation</span>,
+  project: {
+    link: 'https://github.com/digitalfortress-dev/foundation',
+  },
+  docsRepositoryBase: 'https://github.com/digitalfortress-dev/foundation',
+  footer: {
+    text: 'Digital Fortress Foundation',
+  },
+}
+
+export default config
